981fece0f64e32e49e829b13b55be76c466b1d00
tmurray19 <me@taidghmurray.ie>
2019-08-07 10:16:01 +0100

    Adding accessor for project status

--- app/queueMaker/queue_service.py
+++ app/queueMaker/queue_service.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from json import dump
+from json import dump, load
 from os.path import join
 from app import app
 
@@ -16,12 +16,31 @@ def create_queue(proj_id):
     queue_info = {
         "id": proj_id,
         "dateRequested": datetime.now().strftime("%d-%b-%Y (%H:%M:%S)"),
-        "dateCompleted": "",
+        "dateCompleted": "TBA",
         "status": False,
-        "otherInfo": ""
+        "otherInfo": "None"
     }
     # Open and write to file
     with open(join(app.config['DIR_LOCATION'], app.config['QUEUE_FOLDER'], proj_id + "_queue_status.json"), 'w') as outfile:
         dump(queue_info, outfile)
     
     print("Queue file written to {}".format(join(app.config['DIR_LOCATION'], app.config['QUEUE_FOLDER'], proj_id + "_queue_status.json")))
+
+
+
+def get_queue_status(proj_id):
+    try:
+        # Create The JSON File
+        json_file = join(app.config['DIR_LOCATION'], app.config['QUEUE_FOLDER'], proj_id + "_queue_status.json")
+        with open(json_file) as json_file:
+            json_data = load(json_file)
+            status = json_data['status']
+
+    
+    except Exception as e:
+        status = str(e)
+
+    if status == True:
+        return ("Job complete, Video has been rendered.")
+    else:
+        return ("Job incomplete, Video has not been rendered yet.")
--- app/routes.py
+++ app/routes.py
@@ -8,9 +8,18 @@ api = Api(app=app)
 
 @api.route('/render/<string:proj_id>')
 class RenderVideo(Resource):
-    def  get(self, proj_id):
+    def get(self, proj_id):
         p = Process(target=queue_service.create_queue, args=(proj_id,))
         p.start()
         p.join()
         print("Process complete")
         #p.close()
+
+
+@api.route('/sherpatemplatestatus/<string:projectid>')
+class RenderVideoStatus(Resource):
+    def get(self, proj_id):
+        p = Process(target=queue_service.get_queue_status, args=(proj_id,))
+        p.start()
+        p.join()
+        print("Process complete")
