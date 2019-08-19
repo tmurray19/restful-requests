bdcd5c458db0ab0e6512cd688235879c77618b80
tmurray19 <me@taidghmurray.ie>
2019-08-19 10:33:58 +0100

    Cleaned up updatingproject code

--- app/fileShare/null_link.py
+++ app/fileShare/null_link.py
@@ -7,43 +7,13 @@ from app import app
 import pyodbc
 
 
-def upd_projectnullvideolink(proj_id):
+def updateproject(proj_id, nullify=False):
+    """ Set nullify to true to send a null link to the database"""
     try:
-
-        # Set the Connection Parameters
-        params = urllib.parse.quote_plus(
-            "DRIVER={SQL Server Native Client 11.0};"
-            "SERVER="+app.config['DBCONNSERV']+";"
-            "DATABASE="+app.config['DBASE']+";"
-            "UID="+app.config['DBBUUID']+";"
-            "PWD="+app.config['DBPW']+""
-        )
-
-        #
-        # Open DB Connection & Session
-        #
-        engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect={}".format(params))
-        #
-        Session = scoped_session(sessionmaker(bind=engine))
-        #
-        s = Session()
-        # Build the SQL Query
         query = 'UPDATE project SET FinalViedo_URL = null , Soc_FinalViedo_URL =  null ' \
-                +' WHERE [id]=' + str(proj_id)
-        #
-        # Execute & Commit
-        #
-        result = s.execute(query)
-        result = s.execute('COMMIT')
-    except Exception as e:
-        result = str(e)
-
-    return result
-import os
-
-
-def updateproject(projectid):
-    try:
+                +' WHERE [id]=' + str(proj_id) if nullify else 'UPDATE project SET FinalViedo_URL = \''+app.config['EDITORLOC'] + str(proj_id) + '/' \
+                + str(proj_id) + '_edited.mp4\', Soc_FinalViedo_URL = \'https://dev.squarev.mobi/videos/' \
+                + str(proj_id) + '/' + str(proj_id) + '_edited.mp4\' WHERE [id]=' + str(proj_id)
 
         # Set the Connection Parameters
         params = urllib.parse.quote_plus(
@@ -55,22 +25,20 @@ def updateproject(projectid):
         )
 
         # Open DB Connection & Session
-        #
-        engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect={}".format(params))
-        #
+        engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect={}".format(params))      
         Session = scoped_session(sessionmaker(bind=engine))
-        #
         s = Session()
+
         # Build the SQL Query
-        query = 'UPDATE project SET FinalViedo_URL = \''+app.config['EDITORLOC'] + str(projectid) + '/' \
-                + str(projectid) + '_edited.mp4\', Soc_FinalViedo_URL = \'https://dev.squarev.mobi/videos/' \
-                + str(projectid) + '/' + str(projectid) + '_edited.mp4\' WHERE [id]=' + str(projectid)
-        #
+        query = 'UPDATE project SET FinalViedo_URL = \''+app.config['EDITORLOC'] + str(proj_id) + '/' \
+                + str(proj_id) + '_edited.mp4\', Soc_FinalViedo_URL = \'https://dev.squarev.mobi/videos/' \
+                + str(proj_id) + '/' + str(proj_id) + '_edited.mp4\' WHERE [id]=' + str(proj_id)
+        
         # Execute & Commit
-        #
+        
         result = s.execute(query)
         result = s.execute('COMMIT')
     except Exception as e:
         result = str(e)
-
     return result
+
