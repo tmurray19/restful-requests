eae1dd3ccfe67eb07dd661d38653ad6443b4b808
Taidgh Murray <31845904+tmurray19@users.noreply.github.com>
2019-11-05 14:18:01 +0000

    Update config.py

--- config.py
+++ config.py
@@ -20,16 +20,10 @@ class Config(object):
     # Defining storage name and key
     STORAGE_ACCOUNT_NAME = os.environ.get('STORAGE_ACCOUNT_NAME') or 'csae48d5df47deax41bcxbaa'
     STORAGE_ACCOUNT_KEY = os.environ.get('STORAGE_ACCOUNT_KEY') or \
-        'iUTL5cLSDTObfUliySlqjT4x1dfCQ1U7l7zuaZrPEwhGIHnHPKWfYuFrq16cCjFUS/122mcwJpdseC9JI6mSGA=='
+        ''
 
     SHARE_NAME = os.environ.get('SHARE_NAME') or 'cs-william-squarev-media-10037ffe909d3982' 
     # Defining name of json file containing edits
     PROJECT_NAME = os.environ.get('PROJECT_NAME') or 'FinalSubclipJson.json'
 
-    # Refresh locations
 
-    EDITORLOC = 'https://dev.squarev.mobi/videos/'
-    DBCONNSERV = os.environ.get('DBCONNSERV') or 'DEV//SQLEXPRESS'
-    DBASE = os.environ.get('DBASE') or 'ShowWorx'
-    DBBUUID = os.environ.get('DBBUUID') or 'sqv-odbc'
-    DBPW = os.environ.get('DBPW') or 'sqvdev@$365'
